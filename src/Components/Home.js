8a0126f43b341beea63ab0e25fd09b471a47fb8e
Csaba Balogh <csababalogh84@gmail.com>
2019-03-30 22:09:58 -0700

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,27 @@
 import React from "react";
 import { NavLink } from 'react-router-dom'
-import { MDBIcon,  MDBBtn } from "mdbreact";
-
+import { MDBIcon,  MDBBtn, MDBContainer } from "mdbreact";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 import '../view/Home.css';
 
 
 const Home = () => {
   return (
     <div className="bg home">
-                <div className="white-text text-center text-md-left col-md-6 mt-xl-5 mb-5">
+                <div className="white-text text-center text-md-center col-md-6 mt-xl-5 mb-5">
                   <h1 className="h1-responsive font-weight-bold mt-sm-5">
                     Welcome to PAL Distribution{" "}
                   </h1>
                   <hr className="hr-light" />
-                  <h3 className="mb-4">
+                  <h3 className="mb-4 home-text">
                     Industry leader in Warehouse and Distribution.
                   </h3>
-                  <MDBBtn color="blue darken-3" className="btn">
-                  <MDBIcon far icon="address-book"  /> <NavLink style={{color: "white"}} to="contact"> Contact</NavLink>
+                  <div className="home-container">                  <MDBBtn color="blue darken-3" className="contact-button">
+                    <MDBIcon far icon="address-book"  /> <NavLink style={{color: "white"}} to="contact"> Contact</NavLink>
                   </MDBBtn>
+                  </div>
+
                 </div>
 
     </div>
