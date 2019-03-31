d4557d94f91a601ef7c715719aa5db5aaa16fc1b
Csaba Balogh <csababalogh84@gmail.com>
2019-03-30 22:04:48 -0700

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { MDBCard, MDBCardBody,MDBCardText, MDBCardHeader, MDBContainer } from "mdbreact";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 import "../view/about.css";
 
@@ -21,7 +23,7 @@ class About extends Component {
               <MDBCardText className="card-text">
                 <p className="card__paragraph">Providing an array of services including warehousing and distribution, and special projects services. </p>
                 <p className="card__paragraph">Our goal is to provide you with the most competitive pricing and reliable services.
-                    We value your trust in allowing us to provide our services; in turn, we are committed to giving you a seamless process in which you can rely.</p>
+                    We value your trust in allowing us to provide our services; in turn, we are committed to giving you a seamless process in which you can rely.  </p>
                 <p className="card__paragraph">DDI's Customer Service team is here to provide friendly and efficient communication with our customers; or if our customers prefer they can remotely access our system to manage their inventory, or track and trace their shipments for real time status of shipments.</p>
                 <p className="card__paragraph">DDI Distributions  is your valuable partner in your warehouse, and distribution needs; as we move forward in adapting to our customer needs in this ever changing industry.</p>
               </MDBCardText>
