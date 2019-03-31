cd3526db7b02183c8d554e1deebb578d55b7dd2d
Csaba Balogh <csababalogh84@gmail.com>
2019-03-30 22:08:49 -0700

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,6 +2,8 @@ import "../view/contact.css";
 import React, {Component} from 'react';
 import axios from 'axios';
 import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon, MDBInput } from "mdbreact";
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 
 
@@ -18,20 +20,24 @@ class Contact extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    
-    axios.post("https://pdfmy1ykk8.execute-api.us-east-2.amazonaws.com/s2", this.state)
+    if(this.state.name.length < 5 || this.state.email.length < 3  || this.state.message.length < 10) {
+      alert('please fill out all fields')
+    } else {
+      axios.post("https://pdfmy1ykk8.execute-api.us-east-2.amazonaws.com/s2", this.state)
  
-    .then(res => {
-      
-      this.setState({
-      name: "",
-      email: "",
-      subject: "",
-      message: ""
+      .then(res => {
+        
+        this.setState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+        })
       })
-    })
-    .catch(err => alert(err.message))
-
+      .catch(err => alert(err.message))
+  
+    }
+   
     
   }
   handleInput = (e, field) => {
@@ -92,7 +98,7 @@ class Contact extends Component {
                     label="Your message"
                   />
                 </div>
-                <div className="text-center">
+                <div className="text-center-1">
                   <MDBBtn outline color="blue darken-3" type="submit">
                     Send <MDBIcon far icon="paper-plane" className="ml-1" />
                   </MDBBtn>
@@ -104,5 +110,6 @@ class Contact extends Component {
      );
   }
 }
+
 
 export default Contact;
