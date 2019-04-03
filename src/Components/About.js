83d9c24ae1b07085505ddd249b06d572a2b0bac9
JasonCorp84 <csababalogh84@gmail.com>
2019-04-03 15:24:55 -0700

    about page is responsive

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBCard, MDBCardBody,MDBCardText, MDBCardHeader, MDBContainer } from "mdbreact";
+import { MDBCard, MDBCardBody,MDBCardText,MDBRow, MDBCardHeader, MDBContainer } from "mdbreact";
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
@@ -13,24 +13,24 @@ class About extends Component {
   }
   render() { 
     return ( 
-      <div className="about-container">
-        <MDBContainer>
+      <div className="about-wrapper">
+        <MDBContainer fluid className="about-container">
           <MDBCard className="about-card" style={{ width: "100%"}}>
             <MDBCardHeader color="blue darken-3" tag="h3">
-              About Us
+              <div className="h4-responsive"> About Us </div>
             </MDBCardHeader>
             <MDBCardBody>
-              <MDBCardText className="card-text">
-                <p className="card__paragraph">Providing an array of services including warehousing and distribution, and special projects services. </p>
-                <p className="card__paragraph">Our goal is to provide you with the most competitive pricing and reliable services.
-                    We value your trust in allowing us to provide our services; in turn, we are committed to giving you a seamless process in which you can rely.  </p>
-                <p className="card__paragraph">DDI's Customer Service team is here to provide friendly and efficient communication with our customers; or if our customers prefer they can remotely access our system to manage their inventory, or track and trace their shipments for real time status of shipments.</p>
-                <p className="card__paragraph">DDI Distributions  is your valuable partner in your warehouse, and distribution needs; as we move forward in adapting to our customer needs in this ever changing industry.</p>
+              <MDBCardText className="card-text md-6">
+                <h5 className="h5-responsive">Providing an array of services including warehousing and distribution, and special projects services. </h5>
+                <h5 className="h5-responsive">Our goal is to provide you with the most competitive pricing and reliable services.
+                    We value your trust in allowing us to provide our services; in turn, we are committed to giving you a seamless process in which you can rely.  </h5>
+                <h5 className="h5-responsive">DDI's Customer Service team is here to provide friendly and efficient communication with our customers; or if our customers prefer they can remotely access our system to manage their inventory, or track and trace their shipments for real time status of shipments.</h5>
+                <h5 className="h5-responsive">DDI Distributions  is your valuable partner in your warehouse, and distribution needs; as we move forward in adapting to our customer needs in this ever changing industry.</h5>
               </MDBCardText>
             </MDBCardBody>
           </MDBCard>
         </MDBContainer>
-      </div>
+        </div>
      );
   }
 }
