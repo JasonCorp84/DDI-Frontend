09ca5968fbeec3b77bad57046a60a276bf563cb6
JasonCorp84 <csababalogh84@gmail.com>
2019-04-03 17:37:36 -0700

    weather icon added

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import {
   MDBNavbar, MDBNavbarNav, MDBNavItem, MDBNavLink, MDBNavbarToggler, MDBCollapse, MDBIcon
   } from "mdbreact";
@@ -11,19 +11,41 @@ import '../view/Nav.css';
 class Nav extends Component {
   constructor(props) {
     super(props);
-    this.state = { isOpen: false }
+    this.state = { 
+      isOpen: false,
+      weather: "" ,
+      temp: "",
+      icon: ""
+    }
   }
 
   toggleCollapse = () => {
     this.setState({ isOpen: !this.state.isOpen });
   }
 
+  componentDidMount() {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?q=Moreno Valley,us,ca&appid=fe57fc98328d7b115a5b1b1745446662&units=imperial`)
+    .then(res => 
+      this.setState(
+        {
+          weather: res.data['weather'][0]['description'],
+          temp: res.data['main']['temp'],
+          icon: res.data['weather'][0]['icon']
+        }
+        )
+    )
+    .catch(err=> console.log(err.data))
+  }
+
   render() {
     return (
       <div className="App__Navigation">
         <MDBNavbar color="blue darken-3" dark expand="md">
+
           <MDBNavbarToggler onClick={this.toggleCollapse} />
+
           <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+
             <MDBNavbarNav left>
               <MDBNavItem>
                 <MDBNavLink to="/">Home</MDBNavLink>
@@ -41,14 +63,23 @@ class Nav extends Component {
                 <MDBNavLink to="contact">Contact Us</MDBNavLink>
               </MDBNavItem>
             </MDBNavbarNav>
+
             <MDBNavbarNav right>
-              <MDBNavItem>
+            
+            <MDBNavItem>
               <MDBNavLink className="waves-effect waves-light" to="#!">
+             
               <MDBIcon icon="phone" />  (951)242-3400
               </MDBNavLink>
               </MDBNavItem>
+
+              <MDBNavItem>
+              <div id="openweathermap-widget-13"></div>
+            </MDBNavItem>
+
             </MDBNavbarNav>
           </MDBCollapse>
+
         </MDBNavbar>
         </div>
       );
